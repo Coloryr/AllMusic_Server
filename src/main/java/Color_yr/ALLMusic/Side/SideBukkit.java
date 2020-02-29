66521bf4001adb7e49064f33c123bd0975295d69
Coloryr <402067010@qq.com>
2020-02-29 17:15:15 +0800

    Update SideBukkit.java

--- src/main/java/Color_yr/ALLMusic/Side/SideBukkit.java
+++ src/main/java/Color_yr/ALLMusic/Side/SideBukkit.java
@@ -4,9 +4,9 @@ import Color_yr.ALLMusic.ALLMusic;
 import Color_yr.ALLMusic.ALLMusicBukkit;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import net.minecraft.server.v1_12_R1.*;
+import net.minecraft.server.v1_15_R1.*;
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import java.nio.charset.StandardCharsets;
