b1f81ebd5503c1abd50b5eece7ed79e15074c5d7
Coloryr <402067010@qq.com>
2020-05-06 11:11:37 +0800

    Update CommandEX.java

--- src/main/java/Color_yr/AllMusic/Command/CommandEX.java
+++ src/main/java/Color_yr/AllMusic/Command/CommandEX.java
@@ -91,7 +91,7 @@ public class CommandEX {
                     ClickEvent.Action.SUGGEST_COMMAND, "/music select ");
             AllMusic.Side.SendMessage(sender, "§d[AllMusic]§2使用/music hud enable [位置] 启用关闭Hud§e[§n点我§r§e]",
                     ClickEvent.Action.RUN_COMMAND, "/music vv enable");
-            AllMusic.Side.SendMessage(sender, "§d[AllMusic]§2使用/music hud [位置] [x] [y] 设置VV位置",
+            AllMusic.Side.SendMessage(sender, "§d[AllMusic]§2使用/music hud [位置] [x] [y] 设置某个Hud的位置",
                     ClickEvent.Action.SUGGEST_COMMAND, "/music vv ");
         } else if (args[0].equalsIgnoreCase("stop")) {
             AllMusic.Side.ClearHud(Name);
