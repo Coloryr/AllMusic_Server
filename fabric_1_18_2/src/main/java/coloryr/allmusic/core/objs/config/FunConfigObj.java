bda183823f86b72179601fc07bc53a9623d2de82
Color_yr <402067010@qq.com>
2023-03-02 16:58:01 +0800

    fabric ok

---
+++ fabric_1_18_2/src/main/java/coloryr/allmusic/core/objs/config/FunConfigObj.java
@@ -0,0 +1,11 @@
+package coloryr.allmusic.core.objs.config;
+
+public class FunConfigObj {
+    public boolean Rain;
+    public int RainRate;
+
+    public FunConfigObj(){
+        Rain = true;
+        RainRate = 10;
+    }
+}
--- link.cmd
+++ link.cmd
@@ -1,11 +1,14 @@
 mklink /j "server/src/main/java/coloryr/allmusic/core" "core"
 mklink /j "forge_1_19_3/src/main/java/coloryr/allmusic/core" "core"
 mklink /j "fabric_1_19_3/src/main/java/coloryr/allmusic/core" "core"
+mklink /j "fabric_1_18_2/src/main/java/coloryr/allmusic/core" "core"
 
 mklink /j "server/build" "build"
 mklink /j "forge_1_19_3/build" "build"
 mklink /j "fabric_1_19_3/build" "build"
+mklink /j "fabric_1_18_2/build" "build"
 
 mklink /j "server/.gradle" ".gradle"
 mklink /j "forge_1_19_3/.gradle" ".gradle"
 mklink /j "fabric_1_19_3/.gradle" ".gradle"
+mklink /j "fabric_1_18_2/.gradle" ".gradle"
