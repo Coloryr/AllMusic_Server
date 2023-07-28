f5a807424dab8ee7ce0dd78f5bf3e0ce5f9e99fc
Coloryr <402067010@qq.com>
2023-07-28 09:54:16 +0800

    up
    1.19.2支持

---
+++ forge_1_19_2/src/main/java/coloryr/allmusic/Tasks.java
@@ -0,0 +1,33 @@
+package coloryr.allmusic;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Tasks {
+    private static final List<TaskItem> taskItems = new CopyOnWriteArrayList<>();
+
+    public static void init() {
+        AllMusicForge.server.addTickable(Tasks::tick);
+    }
+
+    public static void tick() {
+        synchronized (taskItems) {
+            for(TaskItem item : taskItems){
+                item.tick--;
+                if (item.tick == 0) {
+                    taskItems.remove(item);
+                    item.run.run();
+                }
+            }
+        }
+    }
+
+    public static void add(TaskItem item) {
+        synchronized (taskItems) {
+            taskItems.add(item);
+        }
+    }
+}
