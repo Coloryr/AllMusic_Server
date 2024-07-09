096d943c2920e63a230b06dd7a2e649489c94511
Coloryr <402067010@qq.com>
2024-07-09 11:48:07 +0800

    up

--- fabric_1_21/src/main/java/com/coloryr/allmusic/server/PackPayload.java
+++ fabric_1_21/src/main/java/com/coloryr/allmusic/server/PackPayload.java
@@ -7,7 +7,9 @@ import net.minecraft.network.packet.CustomPayload;
 
 public record PackPayload(ComType type, String data, int data1) implements CustomPayload {
     public static final Id<PackPayload> ID = new CustomPayload.Id<>(AllMusicFabric.ID);
-    public static final PacketCodec<PacketByteBuf, PackPayload> CODEC = PacketCodec.of((value, buf) -> com.coloryr.allmusic.server.codec.PacketCodec.pack(buf, value.type, value.data, value.data1), buffer -> new PackPayload(ComType.CLEAR, null, 0));
+    public static final PacketCodec<PacketByteBuf, PackPayload> CODEC =
+            PacketCodec.of((value, buf) -> com.coloryr.allmusic.server.codec.PacketCodec.pack(buf, value.type, value.data, value.data1),
+                    buffer -> new PackPayload(ComType.CLEAR, null, 0));
 
     @Override
     public Id<? extends CustomPayload> getId() {
