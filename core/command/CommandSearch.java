d981ff28d28659362e54ab5df16f91692cb5da48
Color_yr <402067010@qq.com>
2025-06-29 18:47:08 +0800

    up
    Fixed #57

--- core/command/CommandSearch.java
+++ core/command/CommandSearch.java
@@ -6,7 +6,7 @@ public class CommandSearch extends ACommand {
     @Override
     public void execute(Object sender, String name, String[] args) {
         if (AllMusic.getConfig().needPermission &&
-                !AllMusic.side.checkPermission(name, "allmusic.search")) {
+                !AllMusic.side.checkPermission(sender, "allmusic.search")) {
             AllMusic.side.sendMessage(sender, AllMusic.getMessage().search.noPer);
             return;
         }
