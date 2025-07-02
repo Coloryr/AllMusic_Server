ac0e04befb6350cd05ae631e603d0d887569b566
Color_yr <402067010@qq.com>
2025-07-02 10:05:00 +0800

    up use cleanroom

---
+++ forge_1_12_2/gradle/scripts/extra.gradle
@@ -0,0 +1,5 @@
+// You may write any gradle buildscript component in this file
+// This file is automatically applied after build.gradle + dependencies.gradle is ran
+
+// If you wish to use the default helper methods, uncomment the line below
+// apply from: 'gradle/scripts/helpers.gradle'
