f9972c63b43293ba5d31a96eb9b3fab4b0ef38d8
Bill <53694134+BillZhucn@users.noreply.github.com>
2025-08-28 04:40:20 +0100

    Update CommandLastPage.java

--- core/command/CommandLastPage.java
+++ core/command/CommandLastPage.java
@@ -8,7 +8,7 @@ public class CommandLastPage extends ACommand {
     @Override
     public void execute(Object sender, String name, String[] args) {
         if (AllMusic.getConfig().needPermission &&
-                !AllMusic.side.checkPermission(name, "allmusic.search")) {
+                !AllMusic.side.checkPermission(sender, "allmusic.search")) {
             AllMusic.side.sendMessage(sender, AllMusic.getMessage().search.noPer);
             return;
         }
