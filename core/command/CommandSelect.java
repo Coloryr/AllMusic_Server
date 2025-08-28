5ac37bb2f3c0030b11b22e25a1db26a00ab9a88c
Bill <53694134+BillZhucn@users.noreply.github.com>
2025-08-28 04:38:44 +0100

    Update CommandSelect.java

--- core/command/CommandSelect.java
+++ core/command/CommandSelect.java
@@ -14,7 +14,7 @@ public class CommandSelect extends ACommand {
     @Override
     public void execute(Object sender, String name, String[] args) {
         if (AllMusic.getConfig().needPermission &&
-                !AllMusic.side.checkPermission(name, "allmusic.search")) {
+                !AllMusic.side.checkPermission(sender, "allmusic.search")) {
             AllMusic.side.sendMessage(sender, AllMusic.getMessage().search.noPer);
             return;
         }
