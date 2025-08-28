ca7ddcf2210dc34e3b5d3b03690370dae21bec07
Bill <53694134+BillZhucn@users.noreply.github.com>
2025-08-28 04:40:50 +0100

    Update CommandPush.java

--- core/command/CommandPush.java
+++ core/command/CommandPush.java
@@ -14,7 +14,7 @@ public class CommandPush extends ACommand {
     @Override
     public void execute(Object sender, String name, String[] args) {
         if (AllMusic.getConfig().needPermission &&
-                !AllMusic.side.checkPermission(name, "allmusic.push")) {
+                !AllMusic.side.checkPermission(sender, "allmusic.push")) {
             AllMusic.side.sendMessage(sender, AllMusic.getMessage().push.noPermission);
             return;
         }
