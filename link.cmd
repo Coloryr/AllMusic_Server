82ff049d4e619f2ad36f1702ab85cf85e26d8a0c
Color_yr <402067010@qq.com>
2026-01-30 17:33:53 +0800

    up

--- link.cmd
+++ link.cmd
@@ -34,10 +34,11 @@ set array3=neoforge_1_21_6 fabric_1_21_6 forge_1_20_1 fabric_1_20_1
 for %%i in (%array3%) do (
 
     if not exist "onejar\%%i\build" mkdir "onejar\%%i\build"
-    if not exist "onejar\%%i\src\main\java\com\coloryr\allmusic\client" mkdir "onejar\%%i\src\main\java\com\coloryr\allmusic\client"
     if not exist "onejar\%%i\src\main\resources\com\coloryr\allmusic\client\core\player\decoder" mkdir "onejar\%%i\src\main\resources\com\coloryr\allmusic\client\core\player\decoder"
 
     if not exist "onejar\%%i\build\libs" mklink /j "onejar\%%i\build\libs" "build\libs"
+    if not exist "onejar\%%i\src\main\java\com\coloryr\allmusic\server" mklink /j "onejar\%%i\src\main\java\com\coloryr\allmusic\server" "%%i\src\main\java\com\coloryr\allmusic\server"
+    if not exist "onejar\%%i\src\main\java\com\coloryr\allmusic\client" mklink /j "onejar\%%i\src\main\java\com\coloryr\allmusic\client" "client\%%i\src\main\java\com\coloryr\allmusic\client"
     if not exist "onejar\%%i\src\main\java\com\coloryr\allmusic\server\core" mklink /j "onejar\%%i\src\main\java\com\coloryr\allmusic\server\core" "core"
     if not exist "onejar\%%i\src\main\java\com\coloryr\allmusic\server\codec" mklink /j "onejar\%%i\src\main\java\com\coloryr\allmusic\server\codec" "codec"
     if not exist "onejar\%%i\src\main\java\com\coloryr\allmusic\client\core" mklink /j "onejar\%%i\src\main\java\com\coloryr\allmusic\client\core" "client\core"
