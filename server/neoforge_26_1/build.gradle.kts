5c03fac05875efcc2239dbc3ea8d3dd73b362f3d
Color_yr <402067010@qq.com>
2026-05-08 23:40:53 +0800

    up

--- server/neoforge_26_1/build.gradle.kts
+++ server/neoforge_26_1/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("dev.architectury.loom-no-remap") version Versions.architecturyLoom
-    id("architectury-plugin") version "3.5-SNAPSHOT"
+//    id("architectury-plugin") version "3.5-SNAPSHOT"
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_25
@@ -19,14 +19,14 @@ dependencies {
     minecraft("com.mojang:minecraft:26.1")
     neoForge("net.neoforged:neoforge:26.1.0.19-beta")
 
-//    modImplementation("net.kyori:adventure-platform-neoforge:6.9.0")
-//
-//    shadowImplementation("net.kyori:adventure-text-minimessage:4.26.1")
-//    shadowImplementation("net.kyori:adventure-api:4.26.1")
-//    shadowImplementation("net.kyori:adventure-text-serializer-gson:4.8.1")
-//    shadowImplementation("net.kyori:adventure-text-serializer-legacy:4.8.1")
-//    shadowImplementation("net.kyori:adventure-text-serializer-plain:4.8.1")
-//    shadowImplementation("net.kyori:adventure-key:4.8.1")
+    implementation("net.kyori:adventure-platform-neoforge:6.9.0")
+
+    shadowImplementation("net.kyori:adventure-text-minimessage:4.26.1")
+    shadowImplementation("net.kyori:adventure-api:4.26.1")
+    shadowImplementation("net.kyori:adventure-text-serializer-gson:4.8.1")
+    shadowImplementation("net.kyori:adventure-text-serializer-legacy:4.8.1")
+    shadowImplementation("net.kyori:adventure-text-serializer-plain:4.8.1")
+    shadowImplementation("net.kyori:adventure-key:4.8.1")
 }
 
 tasks {
@@ -41,6 +41,7 @@ tasks {
         relocate("com.google.gson", "com.coloryr.allmusic.libs.com.google.gson")
 
 //        inputFile.set(shadowJar.get().archiveFile)
+
         archiveFileName.set("[neoforge-26.1]AllMusic_Server-${project.version}.jar")
         destinationDirectory.set(file("${parent!!.projectDir}/target"))
     }
